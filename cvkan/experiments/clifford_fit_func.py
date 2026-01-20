b7d3b3b258d2044d9a1784708cf4070ae3139b5d
Matthias Wolff <m_wolf37@uni-muenster.de>
2026-01-21 00:32:04 +0100

    Experiments for higher clifford dims on synthetic funcfit tasks

--- cvkan/experiments/clifford_fit_func.py
+++ cvkan/experiments/clifford_fit_func.py
@@ -1,64 +1,83 @@
 import torch
 from pathlib import Path
 
-from torch.utils import data
 from torch_ga.clifford.algebra import CliffordAlgebra
-import torch_ga
 
-from cvkan.utils.dataloading.create_complex_dataset import create_complex_dataset
+from cvkan.utils.dataloading.create_complex_dataset import load_dataset, move_dataset_to_device, save_dataset
 from cvkan.utils.dataloading.csv_dataloader import CSVDataset
-from cvkan.experiments.fit_formulas import convert_complex_dataset_to_clifford,convert_complex_dataset_to_real
 from cvkan.models.CliffordKAN import CliffordKAN
 from cvkan.utils.loss_functions import MSE, MAE
 from cvkan.experiments.run_crossval import run_crossval
 from cvkan.utils.norm_functions import Norms
 
 _DATASET_SAVEDIR = Path(__file__).parent / "generated_datasets"
-_DEVICE = "cuda"
+_DEVICE = torch.device("cuda")
 
-mse_loss = MSE()
-mae_loss = MAE()
-
-loss_fns = dict()
-loss_fns["mse"] = mse_loss
-loss_fns["mae"] = mae_loss
-
-
-def synthetic_clifford(train_num=5000):
-    # daraset cliff needs to be [B x vars x cliffdim]
-    algebra = CliffordAlgebra(metric=[-1], device=_DEVICE)
+def create_clifford_dataset(name, metric, train_num): 
+    filename = f"clifford_{name}_{metric}_{train_num}.pt"
+    filepath = _DATASET_SAVEDIR / filename
+    if filepath.exists():
+        print(f"Using cached dataset {filepath}")
+        dataset_dict = load_dataset(filepath)
+        move_dataset_to_device(dataset_dict=dataset_dict, device="cuda")
+        return dataset_dict
+    
+    algebra = CliffordAlgebra(metric=metric)
     num_dim =  1 << algebra.num_bases
-    # random samples in range [-2 +2] for each dimension
-    x1 = torch.rand((train_num,1, num_dim)) * 4 - 2
-    x2 = torch.rand((train_num,1, num_dim)) * 4 - 2
-    x1 = x1.to(_DEVICE)
-    x2 = x2.to(_DEVICE)
-    # x1 and x2 have shape B x num_vars x num_dim
-    x1x2 = torch.stack((x1,x2), dim=-1)  # shape B x 1 x num_dim x num_vars
-    x1x2 = x1x2.squeeze(dim=1)  # squeeze 1 dimension away
-    x1x2 = x1x2.permute((0,2,1))  # permute so num_vars is in middle
-    # x1x2 has shape B x num_vars x num_dim
+    if name == "square":
+        # random samples in range [-2 +2] for each dimension
+        x1 = torch.rand((train_num,1, num_dim)) * 4 - 2
+        x1_squared = torch.einsum("bvx,bvy,xyz->bvz", x1, x1, algebra.cayley)
+        dataset_dict = {"train_input": x1, "train_label": x1_squared, "test_input": torch.zeros((0,1)), "test_label": torch.zeros((0,1))}
+    elif name == "squaresquare":
+        x1 = torch.rand((train_num,1, num_dim)) * 4 - 2
+        x2 = torch.rand((train_num,1, num_dim)) * 4 - 2
+        # x1 and x2 have shape B x num_vars x num_dim
+        x1x2 = torch.stack((x1,x2), dim=-1)  # shape B x 1 x num_dim x num_vars
+        x1x2 = x1x2.squeeze(dim=1)  # squeeze 1 dimension away
+        x1x2 = x1x2.permute((0,2,1))  # permute so num_vars is in middle
+        # x1x2 has shape B x num_vars x num_dim
+        #b = batch, v = number of variables in synthetic dataset (input), xyz = clifford dims
+        x1_squared = torch.einsum("bvx,bvy,xyz->bvz", x1, x1, algebra.cayley)
+        x2_squared = torch.einsum("bvx,bvy,xyz->bvz", x2, x2, algebra.cayley)
+        sum_of_squares = x1_squared + x2_squared
+        square_of_squares = torch.einsum("bvx,bvy,xyz->bvz", sum_of_squares, sum_of_squares, algebra.cayley)
+        dataset_dict = {"train_input": x1x2, "train_label": square_of_squares, "test_input": torch.zeros((0,1)), "test_label": torch.zeros((0,1))}
+    elif name == "mult":
+        x1 = torch.rand((train_num,1, num_dim)) * 4 - 2
+        x2 = torch.rand((train_num,1, num_dim)) * 4 - 2
+        x1x2 = torch.stack((x1,x2), dim=-1)  # shape B x 1 x num_dim x num_vars
+        x1x2 = x1x2.squeeze(dim=1)  # squeeze 1 dimension away
+        x1x2 = x1x2.permute((0,2,1))  # permute so num_vars is in middle
+        x1_mult_x2 = torch.einsum("bvx,bvy,xyz->bvz", x1, x2, algebra.cayley)
+        dataset_dict = {"train_input": x1x2, "train_label": x1_mult_x2, "test_input": torch.zeros((0,1)), "test_label": torch.zeros((0,1))}
+    else:
+        raise NotImplementedError(f"Clifford Dataset for name {name} is not defined!")
+    if not filepath.exists():
+        save_dataset(dataset_dict=dataset_dict, filepath=filepath)
+    return dataset_dict
 
-    #b = batch, v = number of variables in synthetic dataset (input), xyz = clifford dims
-    x1_squared = torch.einsum("bvx,bvy,xyz->bvz", x1, x1, algebra.cayley)
-    x2_squared = torch.einsum("bvx,bvy,xyz->bvz", x2, x2, algebra.cayley)
-    sum_of_squares = x1_squared + x2_squared
-    square_of_squares = torch.einsum("bvx,bvy,xyz->bvz", sum_of_squares, sum_of_squares, algebra.cayley)
-    dataset_sq_cliff = {"train_input": x1, "train_label": x1_squared, "test_input": torch.zeros((0,1)), "test_label": torch.zeros((0,1))}
-    dataset_sq_cliff = CSVDataset(dataset_sq_cliff, input_vars=["x1"], output_vars=["x1^2"], categorical_vars=[], train_test="100:0")
 
-    dataset_sqsq_cliff = {"train_input":x1x2 , "train_label": square_of_squares, "test_input": torch.zeros((0,1)), "test_label": torch.zeros((0,1))}
-    dataset_sqsq_cliff = CSVDataset(dataset_sqsq_cliff, input_vars=["x1"], output_vars=["x1^2"], categorical_vars=[], train_test="100:0")
+def synthetic_clifford(name, metric, extra_args, train_num=80000):
+    # 80k points because it was 5k for 2d problems (cv), they have a volume of 16 ([-2,2]^2 = 4^2 = 16)
+    # now we need [-2,2]^4 = 256, so 5k/16 := xk / 256 --> x=80
+    # daraset cliff needs to be [B x vars x cliffdim]
+    norm_to_use = Norms(extra_args["norm"])
+    algebra = CliffordAlgebra(metric=metric)
+    num_dim =  1 << algebra.num_bases
+    input_vars = {"square": ["x1"], "squaresquare": ["x1", "x2"], "mult": ["x1", "x2"]}
+    output_vars = {"square": ["x1^2"], "squaresquare": ["(x1^2+x2^2)^2"], "mult": ["x1 * x2"]}
+    dataset_dict = create_clifford_dataset(name=name, metric=metric, train_num=train_num)
+    dataset_cliff = CSVDataset(dataset_dict, input_vars=input_vars[name], output_vars=output_vars[name], categorical_vars=[], train_test="100:0")
+    archs = {"square": [[1,1],[1,2,1]], "squaresquare": [[2,1,1], [2,4,2,1]], "mult": [[2,2,1], [2,4,2,1]]}
+    for arch in archs[name]:
+        algebra = CliffordAlgebra(metric=metric, device=_DEVICE)
+        cliffkan = CliffordKAN(layers_hidden=arch, algebra=algebra, num_grids=8, rho=1, use_norm=norm_to_use, extra_args=extra_args)
+        mse_loss = MSE(cliffkan.algebra)
+        mae_loss = MAE(cliffkan.algebra)
+        loss_fns = dict()
+        loss_fns["mse"] = mse_loss
+        loss_fns["mae"] = mae_loss
+        loss_fn_backprop = loss_fns["mse"]
+        run_crossval(cliffkan, dataset_cliff, dataset_name=f"clifford_{name}_{metric}_{train_num}", loss_fn_backprop=loss_fn_backprop,loss_fns=loss_fns, batch_size=5000, add_softmax_lastlayer=False, epochs=1000, convert_model_output_to_real=False)
 
-    clifford_extra_args = {"clifford_rbf": "naive", "clifford_grid": "full_grid"}
-
-    for arch in ([2,3,1], ):
-            cliffkan = CliffordKAN(layers_hidden=arch, algebra=algebra, num_grids=8, rho=1, use_norm=Norms.BatchNormComponentWise, clifford_extra_args=clifford_extra_args)
-            loss_fns["mse"] = MSE(ga=cliffkan.algebra)
-            loss_fn_backprop = loss_fns["mse"]
-            run_crossval(cliffkan, dataset_sqsq_cliff, dataset_name="clifford_square", loss_fn_backprop=loss_fn_backprop,loss_fns=loss_fns, batch_size=500, add_softmax_lastlayer=False, epochs=1000, convert_model_output_to_real=False)
-
-
-
-if __name__ == "__main__":
-    synthetic_clifford()
