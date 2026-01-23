c27f7e6c01caa911f7d2cac8bfc3ecd0377f318a
Matthias Wolff <m_wolf37@uni-muenster.de>
2026-01-23 01:25:11 +0100

    Added EarlyStopper class

---
+++ cvkan/utils/early_stopping.py
@@ -0,0 +1,29 @@
+class EarlyMinStopper():
+    def __init__(self, patience, threshold, logging_interval=50):
+        """
+        If EarlyStopper sees no improvement larger than 'threshold' for 'patience' steps, it will return .stop_training() = True
+        :param patience: How many steps to tolerate no improvement for
+        :param threshold: How big the change needs to be at least to be counted as improvement
+        """
+        self.patience = patience
+        self.threshold = threshold
+        self.steps_without_improvement = 0
+        self.best_value = float("inf")
+        self.stop_criterion_reached = False
+        self.logging_interval = logging_interval
+    def step(self, value):
+        if value < self.best_value - self.threshold:
+            # Improvement
+            self.best_value = value
+            self.steps_without_improvement = 0
+        else:
+            # No Improvement
+            self.steps_without_improvement += 1
+            if self.logging_interval > 0 and self.steps_without_improvement % self.logging_interval == 0:
+                print(f"EarlyStopper noticed no improvement in current epoch! Steps without Improvement: {self.steps_without_improvement} / {self.patience}")
+        if self.steps_without_improvement > self.patience:
+            self.stop_criterion_reached = True
+
+    def should_stop(self):
+        return self.stop_criterion_reached
+
